9636bf47565ff5da56f801c1a53ab99743eb62b5
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-05 00:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 window.promises = [];
+let div= document.querSelector("#output");
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const p1 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("P1 is resolve");
+	},1000)
+});
+const p2 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("P2 is resolve");
+	},2000)
+})
+const p3 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("P3 is resolve");
+	},3000)
+})
+
+const p4 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		reject("P4 is resolve");
+	},4000)
+})
+
+const p5 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("P5 is resolve");
+	},5000)
+})
+Promise.any([p1,p2,p3,p4,p5])
+.then((data)=>{
+	div.innerHTML = data;
+})
+.catch((err)=>{
+	div.innerHTML= err
+})
